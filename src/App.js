5e62736c2ee4b5cc080b6467a8ac0ec8753f9c18
gustav osorio <kravitzluis@gmail.com>
2018-08-13 17:33:41 -0500

    Redux store  add data to ListCustom

--- src/App.js
+++ src/App.js
@@ -1,17 +1,38 @@
 import React, { Component } from 'react';
 import './styles.scss';
+import store from './lib/store';
+import ListCustom from './components/listCustom';
+
 class App extends Component {
+  constructor() {
+    super();
+    this.addToList = this.addToList.bind(this);
+     this.state = {
+      items: []
+    }
+  }
+  addItemToList() {
+        //this.setState({ mails: newMails })
+        this.addToList({ item: 'item nuevo' })
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Hola</h1>
+      <div className="app">
+        <header className="app-header">
+          <h1 className="app-title">Lista dinámica</h1>
         </header>
-        <p className="App-intro">
-          Mundo
-        </p>
+        <section className="app-Container">
+          <button onClick={() => this.addItemToList()}>Agregar elemento </button>
+          <ListCustom />
+        </section>
       </div>
     );
+  }
+  addToList(item) {
+    store.dispatch({
+      type: "ADD_TO_LIST",
+      item
+    })
   }
 }
 export default App; 
--- src/lib/store.js
+++ src/lib/store.js
@@ -1,4 +1,14 @@
 import { createStore} from 'redux'
  const reducer = (state, action) => {
+	
+	switch(action.type) {
+    case 'ADD_TO_LIST':
+      return {
+      	...state,
+      	listData: state.listData.concat(action.item)
+      }
+    default:
+      return state
+  	}
 }
-export default createStore(reducer, { listMails: [] }) 
+export default createStore(reducer, { listData: [] }) 
